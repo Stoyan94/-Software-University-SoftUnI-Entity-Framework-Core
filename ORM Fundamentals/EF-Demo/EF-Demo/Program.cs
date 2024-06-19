ff7e30129beee4f55e852847e387e3f9e0fa42fe
Stoyan <podlobg@gmail.com>
2024-06-19 22:17:14 +0300

    up

--- ORM Fundamentals/EF-Demo/EF-Demo/Program.cs
+++ ORM Fundamentals/EF-Demo/EF-Demo/Program.cs
@@ -1,10 +1,31 @@
 ﻿using System.Data.SqlClient;
-using System.Data.SqlTypes;
 
-string connectionString = "Server=STOYAN;Database=SoftUni;Trusted_Connection=True;";
-string query = "SELECT EmployeeId, FirstName, LastName, JobTitle FROM Employees WHERE DepartmentID = @departmentId";
-int departmentId = 7;
+string connectionString = "Server=STOYAN;Database=SoftUni;User Id=sa;Password=558955;Trusted_Connection=True;";
+string query = "SELECT EmployeeID, FirstName, LastName, JobTitle FROM Employees WHERE DepartmentID = @departmentId";
+int departmentId = 3;
 
 using SqlConnection connection = new SqlConnection(connectionString);
 
-SqlCommand cmd = connection.CreateCommand();
+SqlCommand cmd = new SqlCommand(query, connection);
+cmd.Parameters.AddWithValue("@departmentId", departmentId);
+
+try
+{
+    connection.Open();
+
+    SqlDataReader reader = cmd.ExecuteReader();
+
+    while (reader.Read()) 
+    {
+        Console.WriteLine($"{reader[1]} {reader[2]}:  {reader[3]}");
+    }
+
+    Console.WriteLine("yes");
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
+
+
