755a5a0604f50a983e95a12af67c2810739caa36
Stoyan <podlobg@gmail.com>
2024-06-19 21:26:09 +0300

    up

---
+++ ORM Fundamentals/EF-Demo/EF-Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+string connectionString = "Server=STOYAN;Database=SoftUni;Trusted_Connection=True;";
+string query = "SELECT EmployeeId, FirstName, LastName, JobTitle FROM Employees WHERE DepartmentID = @departmentId";
+int departmentId = 7;
+
+using SqlConnection connection = new SqlConnection(connectionString);
+
+SqlCommand cmd = connection.CreateCommand();
