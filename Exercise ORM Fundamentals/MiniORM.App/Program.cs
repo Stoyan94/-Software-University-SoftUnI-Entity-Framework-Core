ecaa129851c8090f245689e04726e8cf52d29abf
Stoyan <podlobg@gmail.com>
2024-06-21 19:48:33 +0300

    up

--- Exercise ORM Fundamentals/MiniORM.App/Program.cs
+++ Exercise ORM Fundamentals/MiniORM.App/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using MiniORM;
+using MiniORM.App.Entities;
+
+var departments = new Department[] { 
+    new Department { Id = 1, Name = "First"}, 
+    new Department { Id = 2, Name = "Second" } };
+
+var changeTracker = new ChangeTracker<Department>(departments);
+
+Console.WriteLine();
