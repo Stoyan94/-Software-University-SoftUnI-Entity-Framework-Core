2cceb53a53311c6d54e698bfc58ae1bab89e6c48
Stoyan <podlobg@gmail.com>
2024-06-25 19:46:09 +0300

    up

--- Exercise Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
+++ Exercise Entity Framework Introduction/SoftUni/SoftUni/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using SoftUni.Data;
+using System.Text;
 
 namespace SoftUni;
 
@@ -8,12 +9,39 @@ public class StartUp
     {
         SoftUniContext dbContext = new SoftUniContext();
 
-        var employees = dbContext.Employees.Where(e=> e.EmployeeId == 1);
+        string result = GetEmployeesFullInformation(dbContext);
+        Console.WriteLine(result);
 
-        foreach ( var employee in employees ) 
-        {
-            Console.WriteLine($"{employee.FirstName}");
-        }
+        //var employees = dbContext.Employees.Where(e=> e.EmployeeId == 1);
+
+        //foreach ( var employee in employees ) 
+        //{
+        //    Console.WriteLine($"{employee.FirstName}");
+        //}
     }
+    public static string GetEmployeesFullInformation(SoftUniContext context)
+    {
+        StringBuilder output = new StringBuilder();
+
+        var employees = context.Employees
+            .OrderBy(e => e.EmployeeId)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.MiddleName,
+                e.JobTitle,
+                e.Salary
+            })
+            .ToArray();
+
+        foreach (var employee in employees)
+        {
+            output.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+        }
+
+        return output.ToString().TrimEnd();
+    }
+
 }
 
