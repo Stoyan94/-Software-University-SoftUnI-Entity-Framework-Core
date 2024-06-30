ab567719392683e3e4d19ed2c7772b6027aa7ed0
Stoyan <podlobg@gmail.com>
2024-06-30 13:21:19 +0300

    up

---
+++ Entity Relations/Blog/Blog/Blog/Blog.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogDemo
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ Entity Relations/Blog/Blog/Blog/BlogDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogDemo;
+
+public class BlogDbContext : DbContext
+{
+    public BlogDbContext()
+    {
+            
+    }
+
+    public BlogDbContext(DbContextOptions<BlogDbContext> options) 
+        : base(options) 
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Blog>()
+            .HasKey(b => b.BlogId);
+
+        modelBuilder.Entity<Blog>()
+            .ToTable("Blogs", "blg");
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+       if (optionsBuilder.IsConfigured == false)
+       {
+            string connectionString = "Server=STOYAN;Database=BlogDb;User Id=sa;Password=558955;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
+
