d2d6ccef436cedf0c0ccd1ea58cd423e587bf76d
Stoyan <podlobg@gmail.com>
2024-07-04 20:54:28 +0300

    up

---
+++ LINQ/SoftUni/SoftUni/EmployeeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUni
+{
+    public class EmployeeDTO
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName{ get; set; } = null!;
+
+        public string JobTitle { get; set; } = null!;
+    }
+}
--- LINQ/SoftUni/SoftUni/StartUp.cs
+++ LINQ/SoftUni/SoftUni/StartUp.cs
@@ -11,26 +11,37 @@ public class StartUp
     {
        using SoftUniContext dbContext = new SoftUniContext();
 
-        // SELECT * FROM Employees WHERE DepartmentId = 1
-        IQueryable<Employee> employees = from employee in dbContext.Employees
-                        where employee.DepartmentId == 1
-                        select employee; // this is classic (old) LINQ
-
-        foreach (var e in employees)
-        {
-            Console.WriteLine(e.FirstName);
-        }
-
-
         var emp = await dbContext.Employees
             .Where(e => e.DepartmentId == 1)
-            .ToListAsync();
+            .Select(e => new EmployeeDTO()
+            {
+                FirstName = e.FirstName,
+                LastName = e.LastName,
+                JobTitle = e.JobTitle
+            }).ToListAsync();
 
         foreach (var e in emp)
         {
-            Console.WriteLine(e.FirstName);
+            Console.WriteLine($"{e.FirstName} {e.LastName} {e.JobTitle}");
         }
+
+        //// SELECT * FROM Employees WHERE DepartmentId = 1
+        //IQueryable<Employee> employees = from employee in dbContext.Employees
+        //                where employee.DepartmentId == 1
+        //                select employee; // this is classic (old) LINQ
+
+        //List<Employee> emp = await employees.ToListAsync();
+
+
+        //List<Employee> emp = await dbContext.Employees
+        //    .Where(e => e.DepartmentId == 1)
+        //    .ToListAsync();
+
+        //foreach (var e in emp)
+        //{
+        //    Console.WriteLine(e.FirstName);
+        //}
     }
-  
+
 }
 
