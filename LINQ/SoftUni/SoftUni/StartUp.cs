fb62eccfa473d2cbae1a4591a7e89e8f799249b4
Stoyan <podlobg@gmail.com>
2024-07-05 20:37:01 +0300

    up

--- LINQ/SoftUni/SoftUni/StartUp.cs
+++ LINQ/SoftUni/SoftUni/StartUp.cs
@@ -11,7 +11,7 @@ namespace SoftUni;
 
 public class StartUp
 {
-    static async void Main(string[] args)
+    static async Task Main(string[] args)
     {
         using SoftUniContext dbContext = new SoftUniContext();
 
@@ -68,9 +68,16 @@ public class StartUp
                     })
         .ToListAsync();
 
-       // While both queries aim to achieve the same result(retrieving employee details along with their department name), 
-       //     Query 2 is preferred due to its simplicity and readability, provided that the Employee entity has a proper navigation property to the Department entity. 
-       //     Both should produce equivalent SQL and perform similarly in most scenarios.
+        var emp2 = await dbContext.Employees
+                   .Where(e => e.DepartmentId == 1) //Include: However, it does not support specifying individual properties of the related entity.
+                                                    //Instead, it is used to include the entire related entity.
+                   .Include(e => e.Department.Name).
+                   ToListAsync();
+
+        // While both queries aim to achieve the same result(retrieving employee details along with their department name), 
+        //     Query 2 is preferred due to its simplicity and readability, provided that the Employee entity has a proper navigation property to the Department entity. 
+        //     Both should produce equivalent SQL and perform similarly in most scenarios.
+        // To know that EF should do Join, we must use Select
 
         foreach (var e in emp)
         {
