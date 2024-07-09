c93cc4e46f6b31664bc9f17f12bdc635f7dc10a6
Stoyan <podlobg@gmail.com>
2024-07-09 19:32:32 +0300

    up

--- Advanced Querying/SoftUni/SoftUni/StartUp.cs
+++ Advanced Querying/SoftUni/SoftUni/StartUp.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using SoftUni.Data;
+using SoftUni.Models;
+using System.Drawing;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 
 namespace SoftUni;
@@ -10,14 +13,49 @@ public class StartUp
     static async Task Main(string[] args)
     {
         using SoftUniContext dbContext = new SoftUniContext();
-        await ExplicitLoading(dbContext);
 
-        
+        var employees = await dbContext.Employees
+            .Where(e => e.DepartmentId == 1)
+            .Include(e => e.Department)
+            .ToListAsync();
 
-        ExecutingStoredProcedure(dbContext);
+        //await ExplicitLoading(dbContext);
+        //ExecutingStoredProcedure(dbContext);
         //await Console.Out.WriteLineAsync(await SQLInjectionDefense(dbContext));
     }
 
+    private static async Task ExplicitAndEagerLoadingIssue(SoftUniContext dbContext)
+    {
+        var employees = await dbContext.Employees
+            .Where(e => e.DepartmentId == 1)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                DepartmentName = e.Department.Name,
+            })
+            .ToListAsync();
+        //.Include(e => e.Departments);
+
+        var neesh = dbContext.Entry(employees);
+
+        // Employees table projecting the result into an anonymous object containing FirstName, LastName, and DepartmentName. 
+        //     The issue with this code lies in how you're attempting to access e.Department.
+        //     Name within the projection. To understand why eager or explicit loading cannot be used directly in this context.
+        // 
+        // Reasons:
+        //         Lack of Eager Loading Setup:
+        // 
+        //         The Select method immediately creates a projection, which means the query will only include the columns specified in the projection. 
+        //         At this point, there's no instruction to include the related Department entity.
+
+        // Projection Limits:  
+        //      When you project into an anonymous type, the Entity Framework cannot automatically include related entities because it is not aware of the need to include them before the projection.
+        // 
+        // Explicit Loading Not Available:        
+        //      Explicit loading(.Load()) requires an already existing entity to load related data into.
+        //      Since you're directly projecting into an anonymous type, there's no entity on which to call the Load method.
+    }
     private static async Task ExplicitLoading(SoftUniContext dbContext)
     {
         var employee = await dbContext.Employees.FindAsync(1);
