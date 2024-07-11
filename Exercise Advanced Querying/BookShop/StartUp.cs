309b85c34e886c10f9db4334baaf85d07a08c8a8
Stoyan <podlobg@gmail.com>
2024-07-11 20:46:06 +0300

    up

--- Exercise Advanced Querying/BookShop/StartUp.cs
+++ Exercise Advanced Querying/BookShop/StartUp.cs
@@ -1,14 +1,63 @@
 ﻿namespace BookShop
 {
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Text;
 
     public class StartUp
     {
         public static void Main()
         {
-            using var db = new BookShopContext();
-            DbInitializer.ResetDatabase(db);
+            using var dbContext = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+            string result = GetBooksByAgeRestriction(dbContext, input);
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext dbContext, string command)
+        {
+            //bool hasparsed = enum.tryparse(typeof(agerestriction), command, true, out object? agerestrictionobj);
+            //agerestriction agerestriction;
+
+            //if (hasparsed)
+            //{
+            //    agerestriction = (agerestriction)agerestrictionobj!;
+
+            //    string[] booktitles = dbcontext.books
+            //    .where(b => b.agerestriction == agerestriction)
+            //    .orderby(b => b.title)
+            //    .select(b => b.title)
+            //    .toarray();
+
+            //    return string.join(environment.newline, booktitles);
+            //}
+
+            //return null;
+
+
+            try
+            {
+                AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+                string[] bookTitles = dbContext.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToArray();
+
+                return string.Join(Environment.NewLine, bookTitles);
+
+            }
+            catch (Exception)
+            {
+
+                return "Not Found";
+            }
+
         }
     }
 }
