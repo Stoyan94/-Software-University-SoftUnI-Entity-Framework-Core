95824c996f65a1f39f2e811e28b0c2cac59073bc
Stoyan <podlobg@gmail.com>
2024-07-15 20:51:17 +0300

    up

---
+++ Exercise Migrations in Entity Framework Core/Project_App/AcademicRecordsApp/AcademicRecordsApp/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AcademicRecordsApp.Models
+{
+    public partial class Student
+    {
+        public Student()
+        {
+            Grades = new HashSet<Grade>();
+        }
+
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+
+        public virtual ICollection<Grade> Grades { get; set; }
+    }
+}
