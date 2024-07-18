c407482a8dbfc50568e9c2c5f907ac8b79880b2c
Stoyan <podlobg@gmail.com>
2024-07-18 17:24:13 +0300

    up

---
+++ JSON Processing/JSON.System.Tex PROCESSING/JSON-Demo/JSON-Demo/Newtonsoft-JSON.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace JSON_Demo;
+
+public class Newthon_JSON
+{    
+    static void Main(string[] args)
+    {
+        var person = new Person()
+        {
+            FullName = "Petar Petrov",
+            Age = 25,
+            Height = 185,
+            Weight = 83.7
+        };
+
+        var settings = new JsonSerializerSettings()
+        {
+
+        };
+
+        string data = JsonConvert.SerializeObject(person);
+
+        Console.WriteLine(data);
+
+        Person? person1 = JsonConvert.DeserializeObject<Person>(data);
+
+        Console.WriteLine($"{person1.FullName} is {person1.Age} years old and is {person1.Height}cm high!");
+    }
+}
