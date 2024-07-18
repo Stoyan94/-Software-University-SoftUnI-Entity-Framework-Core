42c953cb8a423d13391cea37a5be40fc75b2dd8c
Stoyan <podlobg@gmail.com>
2024-07-18 16:13:53 +0300

    up

---
+++ JSON Processing/JSON PROCESSING/JSON-Demo/JSON-Demo/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace JSON_Demo;
+
+public class Person
+{
+    public string FullName { get; set; } = null!;
+    public int Age { get; set; }
+    public int Height { get; set; }
+    public double Weight { get; set; }
+}
---
+++ JSON Processing/JSON PROCESSING/JSON-Demo/JSON-Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace JSON_Demo;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new Person()
+        {
+            FullName = "Petar Petrov",
+            Age = 25,
+            Height = 185,
+            Weight = 83.7
+        };
+
+        var options = new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+        
+        string data = JsonSerializer.Serialize(person, options);
+
+        Console.WriteLine(data);
+
+        Person? person1 = JsonSerializer.Deserialize<Person>(data, options);
+
+        Console.WriteLine($"{person1.FullName} is {person1.Age} years old and is {person1.Height} cm high!");
+    }
+
+}
