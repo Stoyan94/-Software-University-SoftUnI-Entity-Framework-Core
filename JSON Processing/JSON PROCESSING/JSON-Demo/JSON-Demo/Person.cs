01c83fec69ee8030b1ec7be0ae35283f62398531
Stoyan <podlobg@gmail.com>
2024-07-18 16:22:09 +0300

    up

--- JSON Processing/JSON PROCESSING/JSON-Demo/JSON-Demo/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JSON_Demo;
-
-public class Person
-{
-    public string FullName { get; set; } = null!;
-    public int Age { get; set; }
-    public int Height { get; set; }
-    public double Weight { get; set; }
-}
