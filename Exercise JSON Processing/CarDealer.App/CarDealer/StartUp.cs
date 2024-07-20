684aed260144f75af69bf3fb846904d374fc7c85
Stoyan <podlobg@gmail.com>
2024-07-20 17:37:39 +0300

    up

--- Exercise JSON Processing/CarDealer.App/CarDealer/StartUp.cs
+++ Exercise JSON Processing/CarDealer.App/CarDealer/StartUp.cs
@@ -12,7 +12,7 @@ namespace CarDealer
             using CarDealerContext dbContext = new CarDealerContext();
 
             string readJsonImportFile = File.ReadAllText("../../../Datasets/parts.json");
-            Console.WriteLine(ImportSuppliers(dbContext, readJsonImportFile));
+            Console.WriteLine(ImportParts(dbContext, readJsonImportFile));
         }
 
         public static string ImportSuppliers(CarDealerContext dbContext, string inputJson)
@@ -25,6 +25,22 @@ namespace CarDealer
             return $"Successfully imported {suppliers.Count}.";
         }
 
-       
+        public static string ImportParts(CarDealerContext dbContext, string inputJson) 
+        { 
+            var parts = JsonConvert.DeserializeObject<List<Part>>(inputJson);
+
+            var validSuppliersId = dbContext.Suppliers
+                .Select(s => s.Id)
+                .ToArray();
+
+            var partsWithValidSuppId = parts.
+                Where(p => validSuppliersId.Contains(p.SupplierId))
+                .ToArray();
+
+            dbContext.Parts.AddRange(partsWithValidSuppId);
+            dbContext.SaveChanges();
+
+            return $"Successfully imported {partsWithValidSuppId.Length}."; 
+        }
     }
 }
