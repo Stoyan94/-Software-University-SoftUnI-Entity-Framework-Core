9dd0d06783ea115406556e17536ff4a39a82c01a
Stoyan <podlobg@gmail.com>
2024-07-20 17:16:00 +0300

    up

---
+++ Exercise JSON Processing/CarDealer.App/CarDealer/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string suppliersString = File.ReadAllText("../../../Datasets/suppliers.json");
+            Console.WriteLine();
+        }
+    }
+}
