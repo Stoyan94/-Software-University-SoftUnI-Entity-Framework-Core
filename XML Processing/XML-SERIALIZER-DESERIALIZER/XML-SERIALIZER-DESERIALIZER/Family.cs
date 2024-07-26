c9d89ed07bba3a43d8a67a49899e969404955960
Stoyan <podlobg@gmail.com>
2024-07-26 21:23:50 +0300

    up

--- XML Processing/XML-SERIALIZER-DESERIALIZER/XML-SERIALIZER-DESERIALIZER/Family.cs
+++ XML Processing/XML-SERIALIZER-DESERIALIZER/XML-SERIALIZER-DESERIALIZER/Family.cs
@@ -1,9 +1,13 @@
-﻿namespace XML_SERIALIZER_DESERIALIZER
+﻿using System.Xml.Serialization;
+
+namespace XML_SERIALIZER_DESERIALIZER
 {
     public class Family
     {
+        [XmlAttribute("name")]
         public string FamilyName { get; set; }
 
+        [XmlArray("FamilyMembers")]
         public Person[] Members { get; set; }
     }
 }
