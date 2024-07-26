f2fd58c60ca28a0f799048cbd36a3cf30261868b
Stoyan <podlobg@gmail.com>
2024-07-26 20:38:53 +0300

    up

--- XML Processing/XMP-DEMO/XML-DEMO/Program.cs
+++ XML Processing/XMP-DEMO/XML-DEMO/Program.cs
@@ -1,8 +1,82 @@
-﻿using System.Xml.Linq;
+﻿using System.Diagnostics.Metrics;
+using System.Reflection;
+using System.Text;
+using System;
+using System.Xml.Linq;
+
+string xml = @"<?xml version=""1.0""?>
+                <library name=""Developer's Library"">
+                 <book>
+                    <title>Professional C# and .NET</title>
+                    <author>Christian Nagel</author>
+                    <isbn>978-0-470-50225-9</isbn>
+                 </book>
+                 <book>
+                    <title>Teach Yourself XML in 10
+                    Minutes</title>
+                    <author>Andrew H. Watt</author>
+                    <isbn>978-0-672-32471-0</isbn>
+                 </book>
+                </library>";
+
+XDocument doc = XDocument.Parse(xml);
+int level = 0;
 
 
+PrintStructure(doc.Elements(), level);
+static void PrintStructure(IEnumerable<XElement> elements, int level)
+{
+    int newLevel = ++level;
 
-// Create XML-File
+    if (elements.Count() == 0)
+    {        
+        return;
+    }
+
+    foreach (XElement element in elements)
+    {
+        Console.WriteLine($"{new string (' ', newLevel*2)}{element.Name}");
+        PrintStructure(element.Elements(), newLevel);
+    }
+}
+
+void LINQ_TO_XML()
+{
+    // LINQ TO XML
+
+    Console.WriteLine(doc.Root.Value); // All elements in all roots
+
+    Console.WriteLine(doc.Root.Descendants()
+        .First()
+        .Elements()
+        .First().Value); // return => Professional C# and .NET - First element from the first Child with LINQ
+
+    string authorName = doc.Root.Descendants()
+                                .First(e => e.Name == "author").Value;
+
+    Console.WriteLine(authorName); // return => Christian Nagel
+
+    doc.Root.Descendants()
+            .First()
+            .SetElementValue("issueDate", "2024-07-25");
+
+    doc.Save("test.xml"); // add Element in the first child element => <book>
+                                                                        // <title>Professional C# and .NET</title>
+                                                                        // <author>Christian Nagel</author>
+                                                                        // <isbn>978-0-470-50225-9</isbn>
+                                                                        // <issueDate>2024-07-25</issueDate>
+
+    doc.Root.Descendants()
+            .First()
+            .SetAttributeValue("issueDate", "2024-07-25");
+
+    doc.Save("test.xml"); // add Attribute in the first child element => <book issueDate="2024-07-25">
+                                                                         // <title>Professional C# and .NET</title>
+                                                                         // <author>Christian Nagel</author>
+                                                                         // <isbn>978-0-470-50225-9</isbn>
+
+
+    // Create XML-File
 //XDocument doc2 = new XDocument();
 
 //doc2.Add(
@@ -36,56 +110,13 @@ car.Add(new XElement("maker", "Renault"));
 car.Add(new XElement("model", "Megan"));
 
 doc3.Save("cars.xml");
-
-void LINQ_TO_XML()
-{
-    string xml = @"<?xml version=""1.0""?>
-<library name=""Developer's Library"">
- <book>
-    <title>Professional C# and .NET</title>
-    <author>Christian Nagel</author>
-    <isbn>978-0-470-50225-9</isbn>
- </book>
- <book>
-    <title>Teach Yourself XML in 10
-    Minutes</title>
-    <author>Andrew H. Watt</author>
-    <isbn>978-0-672-32471-0</isbn>
- </book>
-</library>";
-
-    XDocument doc = XDocument.Parse(xml);
-
-    // LINQ TO XML
-
-    Console.WriteLine(doc.Root.Value); // All elements in all roots
-
-    Console.WriteLine(doc.Root.Descendants()
-        .First()
-        .Elements()
-        .First().Value); // return => Professional C# and .NET - First element from the first Child with LINQ
-
-    string authorName = doc.Root.Descendants()
-                                .First(e => e.Name == "author").Value;
-
-    Console.WriteLine(authorName); // return => Christian Nagel
-
-    doc.Root.Descendants()
-            .First()
-            .SetElementValue("issueDate", "2024-07-25");
-
-    doc.Save("test.xml"); // add Element in the first child element => <book>
-                                                                        // <title>Professional C# and .NET</title>
-                                                                        // <author>Christian Nagel</author>
-                                                                        // <isbn>978-0-470-50225-9</isbn>
-                                                                        // <issueDate>2024-07-25</issueDate>
-
-    doc.Root.Descendants()
-            .First()
-            .SetAttributeValue("issueDate", "2024-07-25");
-
-    doc.Save("test.xml"); // add Attribute in the first child element => <book issueDate="2024-07-25">
-                                                                         // <title>Professional C# and .NET</title>
-                                                                         // <author>Christian Nagel</author>
-                                                                         // <isbn>978-0-470-50225-9</isbn>
+//<?xml version = "1.0" encoding = "utf-8"?>
+//<cars>
+//  <car>
+//    <maker> Renault </maker>
+//    <model> Megan </model>
+//  </car>
+//</cars>
 }
+
+
