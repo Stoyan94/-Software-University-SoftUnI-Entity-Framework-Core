0c0bd91bc77615b4af133899410a8a3c22a608ac
Stoyan <podlobg@gmail.com>
2024-07-27 14:20:14 +0300

    up

---
+++ XML Exerrcise/CarDealer/DTOs/Import/SupplierImportDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("Supplier")]
+    public class SupplierImportDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("isImporter")]
+        public bool isImporter { get; set; }
+    }
+}
--- XML Exerrcise/CarDealer/StartUp.cs
+++ XML Exerrcise/CarDealer/StartUp.cs
@@ -1,10 +1,45 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using System.Xml.Serialization;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            using CarDealerContext dbContext = new CarDealerContext();
 
+            string readInputFile = File.ReadAllText("../../../Datasets/suppliers.xml");
+            Console.WriteLine(ImportSuppliers(dbContext, readInputFile));
+        }
+
+        //9
+        public static string ImportSuppliers(CarDealerContext dbContext, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(SupplierImportDto[])
+                                         ,new XmlRootAttribute("Suppliers"));
+
+            SupplierImportDto[] suppliersDto;
+
+            using (var reader = new StringReader(inputXml))
+            {
+                suppliersDto = (SupplierImportDto[])xmlSerializer.Deserialize(reader);
+            }
+
+            Supplier[] suppliers = suppliersDto
+                .Select(dto => new Supplier()
+                {
+                    Name = dto.Name,
+                    IsImporter = dto.isImporter
+                })
+                .ToArray();
+
+            dbContext.Suppliers.AddRange(suppliers);
+            dbContext.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
         }
     }
 }
