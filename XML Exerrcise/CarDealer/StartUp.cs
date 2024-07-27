51200b08cc5d70fb65788e2a4cb0542ac8c0d351
Stoyan <podlobg@gmail.com>
2024-07-27 18:02:17 +0300

    up

--- XML Exerrcise/CarDealer/StartUp.cs
+++ XML Exerrcise/CarDealer/StartUp.cs
@@ -11,8 +11,8 @@ namespace CarDealer
         {
             using CarDealerContext dbContext = new CarDealerContext();
 
-            string readInputFile = File.ReadAllText("../../../Datasets/suppliers.xml");
-            Console.WriteLine(ImportSuppliers(dbContext, readInputFile));
+            string readInputFile = File.ReadAllText("../../../Datasets/parts.xml");
+            Console.WriteLine(ImportParts(dbContext, readInputFile));
         }
 
         //9
@@ -40,6 +40,41 @@ namespace CarDealer
             dbContext.SaveChanges();
 
             return $"Successfully imported {suppliers.Length}";
+        }
+
+        public static string ImportParts(CarDealerContext dbContext, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(PartImportDto[])
+                                         ,new XmlRootAttribute("Parts"));
+
+            PartImportDto[] partsDto;
+
+            using (var reader = new StringReader(inputXml))
+            {
+                partsDto = (PartImportDto[])xmlSerializer.Deserialize(reader);
+            }
+
+            var supplierId = dbContext.Suppliers
+                .Select(s => s.Id);
+
+            PartImportDto[] partsWithValidSuppliers = partsDto
+                .Where(p => supplierId.Contains(p.SupplierId))
+                .ToArray();
+
+            Part[] parts = partsWithValidSuppliers
+                .Select(dto => new Part()
+                {
+                    Name= dto.Name,
+                    Price= dto.Price,
+                    Quantity= dto.Quantity,
+                    SupplierId = dto.SupplierId
+                })
+                .ToArray();
+
+            dbContext.Parts.AddRange(parts);
+            dbContext.SaveChanges();
+
+            return $"Successfully imported {parts.Length}"; ;
         }
     }
 }
