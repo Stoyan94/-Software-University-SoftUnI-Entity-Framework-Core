0b5c8a611dbbc642c062050b73e82ec688c48c19
Stoyan <podlobg@gmail.com>
2024-07-30 20:51:24 +0300

    up

---
+++ Exam Prep/Exam Prep– 11 April 2023/Invoices/Data/DataConstraints.cs
@@ -0,0 +1,8 @@
+﻿namespace Invoices.Data
+{
+    public static class DataConstraints
+    {
+        public const byte ProductNameMinLength = 9;
+        public const byte ProductNameMaxLength = 10;
+    }
+}
---
+++ Exam Prep/Exam Prep– 11 April 2023/Invoices/Data/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using Invoices.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Invoices.Data.Models
+{
+    using static DataConstraints;
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(ProductNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public CategoryType CategoryType { get; set; }
+    }
+}
