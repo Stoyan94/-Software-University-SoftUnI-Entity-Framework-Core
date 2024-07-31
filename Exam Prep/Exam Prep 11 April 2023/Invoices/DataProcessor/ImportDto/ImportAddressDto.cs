b33d3fa44269538eb8a05bbc5d80502c2b08e9eb
Stoyan <podlobg@gmail.com>
2024-07-31 22:35:23 +0300

    up

--- Exam Prep/Exam Prep 11 April 2023/Invoices/DataProcessor/ImportDto/ImportAddressDto.cs
+++ Exam Prep/Exam Prep 11 April 2023/Invoices/DataProcessor/ImportDto/ImportAddressDto.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Invoices.Data.Models;
+using System.Xml.Serialization;
 
 namespace Invoices.DataProcessor.ImportDto
 {
-    internal class ImportAddressDto
+    [XmlType(nameof(Address))]
+    public class ImportAddressDto
     {
+        [XmlElement(nameof(StreetName))]
+        public string StreetName { get; set; }
+
+        [XmlElement(nameof(StreetNumber))]
+        public int StreetNumber { get; set; }
+
+        [XmlElement(nameof(PostCode))]
+        public string PostCode { get; set; }
+
+        [XmlElement(nameof(City))]
+        public string City { get; set; }
+
+        [XmlElement(nameof(Country))]
+        public string Country { get; set; }
     }
 }
--- Exam Prep/Exam Prep 11 April 2023/Invoices/DataProcessor/ImportDto/ImportClientDto.cs
+++ Exam Prep/Exam Prep 11 April 2023/Invoices/DataProcessor/ImportDto/ImportClientDto.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Invoices.Data.Models;
+using System.Xml.Serialization;
 
 namespace Invoices.DataProcessor.ImportDto
 {
-    internal class ImportClientDto
+    [XmlType(nameof(Client))]
+    public class ImportClientDto
     {
+        [XmlElement(nameof(Name))]
+        public string Name { get; set; }
+
+        [XmlElement(nameof(NumberVat))]
+        public string NumberVat { get; set; }
+
+        [XmlArray(nameof(Addresses))]
+        public ImportAddressDto[] Addresses { get; set; }
     }
 }
