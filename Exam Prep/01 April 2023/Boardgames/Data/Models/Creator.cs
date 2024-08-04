754bd2c51c7486b15507b5fd1bc95d2b217784b8
Stoyan <podlobg@gmail.com>
2024-08-04 14:00:45 +0300

    up

---
+++ Exam Prep/01 April 2023/Boardgames/Data/Models/Creator.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Boardgames.Data.Models
+{
+    using static DataConstraints;
+    public class Creator
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(CreatorFirstNameMaxLenght)]
+        public string FirstName { get; set; } = null!;
+
+        [MaxLength(CreatorLastNameMaxLenght)]
+        public string LastName { get; set; } = null!;
+
+        // TODO: Add nav prop
+    }
+}
---
+++ Exam Prep/01 April 2023/Boardgames/Data/Models/Seller.cs
@@ -0,0 +1,22 @@
+﻿namespace Boardgames.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using static DataConstraints;
+    public class Seller
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(SellerNameMaxLenght)]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(SellerAddressMaxLenght)]
+        public string Address { get; set; } = null!;
+
+        public string Country { get; set; } = null!;
+
+        public string Website { get; set; } = null!;
+
+        // TOD: Add nav prop
+    }
+}
