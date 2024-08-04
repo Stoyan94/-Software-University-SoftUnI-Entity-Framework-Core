556462bbeb4258fbf3e881e9ff63f4e2dfecfab7
Stoyan <podlobg@gmail.com>
2024-08-04 13:18:11 +0300

    up

---
+++ Exam Prep/01 April 2023/Boardgames/Data/Models/Boardgame.cs
@@ -0,0 +1,25 @@
+﻿using Boardgames.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Boardgames.Data.Models
+{
+    using static DataConstraints;
+    public class Boardgame
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(BoardGameNameMinLenght)]
+        public string Name { get; set; } = null!;
+
+        public double Rating { get; set; }
+
+        public int YearPublished { get; set; }
+
+        public CategoryType CategoryType  { get; set; }
+
+        public string Mechanics { get; set; } = null!;
+
+        // TODO: Add nav prop
+    }
+}
---
+++ Exam Prep/01 April 2023/Boardgames/Data/Models/DataConstraints.cs
@@ -0,0 +1,13 @@
+﻿namespace Boardgames.Data.Models
+{
+    public static class DataConstraints
+    {
+        // BoardGame
+        public const byte BoardGameNameMinLenght = 10;
+        public const byte BoardGameNameMaxLenght = 20;
+        public const double BoardGameRatingMinRange = 1.00;
+        public const double BoardGameRatingMaxRange = 10.00;
+        public const int BoardGameYearPublishedMinRange = 2018;
+        public const int BoardGameYearPublishedMaxRange = 2023;
+    }
+}
