53fb1570a1fae42d31e777e9a3b877649cb8c5b7
Stoyan <podlobg@gmail.com>
2024-10-10 17:22:18 +0300

    up

--- Entity Framework Introduction/DBFirst-Demo/Command in package manager console.cs
+++ Entity Framework Introduction/DBFirst-Demo/Command in package manager console.cs
@@ -1,6 +1,6 @@
 The Scaffold-DbContext command is used in Entity Framework Core to reverse engineer an existing database 
     and create the corresponding entity models and a DbContext class in your project. 
-    This command generates code based on the schema of the specified database. Here's a detailed breakdown of the command you provided:
+    This command generates code based on the schema of the specified database. Here's a detailed breakdown of the provided command:
 
 
 Scaffold-DbContext "Server=STOYAN;Database=SoftUni;User Id=sa;Password=558955;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -DataAnnotations -Context SoftUniDbContext -ContextDir Data -OutputDir Data/Models
@@ -13,36 +13,32 @@ This is the command to scaffold(generate) a DbContext and entity classes for a s
 Microsoft.EntityFrameworkCore.SqlServer:
 
 This specifies the provider to be used for the SQL Server database. Entity Framework Core supports multiple database providers, and this one is for SQL Server.
+
 -DataAnnotations:
 
 This option specifies that the generated classes should use data annotations to configure the model. 
     Data annotations are attributes you can apply directly to your classes and properties to configure how they map to the database.
 
 -Context SoftUniDbContext:
-
 This specifies the name of the DbContext class to be generated. In this case, the class will be named SoftUniDbContext.
+
 -ContextDir Data:
-
 This specifies the directory where the DbContext class should be placed. The SoftUniDbContext class will be generated in the Data directory.
--OutputDir Data/Models:
 
+-OutputDir Data/Models:
 This specifies the directory where the entity classes should be placed. The entity classes representing the database tables will be generated in the Data/Models directory.
 
 Example of the Resulting Files
 After running this command, you can expect to have the following structure in your project:
 
-arduino
-Copy code
+
 YourProject
 ?
-??? Data
-?   ??? SoftUniDbContext.cs        // The DbContext class
-?   ??? Models
-?       ??? Department.cs          // Entity class for a table in the database
-?       ??? Employee.cs            // Another entity class
-?       ??? ...                    // Other entity classes
-?
-??? ...
+folder \Data
+file in foldr Data => SoftUniDbContext.cs        // The DbContext class
+folder /Models
+files in folder Models => Department.cs           // Entity class for a table in the database
+                          Employee.cs            // Another entity class
 
 
 Summary
