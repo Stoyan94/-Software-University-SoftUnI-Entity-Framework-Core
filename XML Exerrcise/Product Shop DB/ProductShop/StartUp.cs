117ed5e993de641a90a16bbed3706bb84ceb764f
Stoyan <podlobg@gmail.com>
2024-12-25 15:07:46 +0200

    up

---
+++ XML Exerrcise/Product Shop DB/ProductShop/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using ProductShopContext dbContext = new ProductShopContext();
+
+            var inputFiles = File.ReadAllText("../../../Datasets/users.xml");
+
+            Console.WriteLine(ImportUsers(dbContext, inputFiles));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {      
+            var userDtos = XmlHelper.Deserialize<List<UsersImportDto>>(inputXml, "Users");            
+
+            List<User> users = userDtos
+                .Select(dto => new User()
+                {
+                    FirstName = dto.FirstName,
+                    LastName = dto.LastName,
+                    Age = dto.Age,
+                })
+                .ToList();
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }        
+       
+    }
+}
---
+++ XML Exerrcise/Product Shop DB/ProductShop/XmlHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public static class XmlHelper
+    {
+
+        // Generic XML deserializer
+        public static T Deserialize<T>(string inputXml, string rootName)
+        {
+            // Create an XmlRootAttribute with the specified root name
+            XmlRootAttribute root = new XmlRootAttribute(rootName);
+
+            // Initialize the XmlSerializer with the type and root
+            XmlSerializer serializer = new XmlSerializer(typeof(T), root);
+
+            // Use StringReader to treat the inputXml string as a readable stream
+            using StringReader reader = new StringReader(inputXml);
+
+            // Deserialize the XML into an object of type T
+            T dtos = (T)serializer.Deserialize(reader);
+
+            // Return the deserialized object
+            return dtos;
+        }
+    }
+}
