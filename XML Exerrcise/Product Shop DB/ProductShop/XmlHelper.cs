60dc30a99ea6be5eec4629acd1ce05e09f1cc987
Stoyan <podlobg@gmail.com>
2024-12-26 16:01:11 +0200

    up

--- XML Exerrcise/Product Shop DB/ProductShop/XmlHelper.cs
+++ XML Exerrcise/Product Shop DB/ProductShop/XmlHelper.cs
@@ -1,4 +1,7 @@
-﻿using System.Xml.Serialization;
+﻿using System.Globalization;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace ProductShop
 {
@@ -22,6 +25,38 @@ namespace ProductShop
 
             // Return the deserialized object
             return dtos;
+        }
+
+
+        public static string SerializeToXml<T>(T dto, string xmlRootAttribute, bool omitDeclaration = false)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttribute));
+            StringBuilder stringBuilder = new StringBuilder();
+
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                OmitXmlDeclaration = omitDeclaration,
+                Encoding = new UTF8Encoding(false),
+                Indent = true
+            };
+
+            using (StringWriter stringWriter = new StringWriter(stringBuilder, CultureInfo.InvariantCulture))
+            using (XmlWriter xmlWriter = XmlWriter.Create(stringWriter, settings))
+            {
+                XmlSerializerNamespaces xmlSerializerNamespaces = new XmlSerializerNamespaces();
+                xmlSerializerNamespaces.Add(string.Empty, string.Empty);
+
+                try
+                {
+                    xmlSerializer.Serialize(xmlWriter, dto, xmlSerializerNamespaces);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
