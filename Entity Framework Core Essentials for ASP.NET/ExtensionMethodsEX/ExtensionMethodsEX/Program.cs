f6b59a52a9fd2d7142b356a2bbef781562d56d0d
Stoyan <podlobg@gmail.com>
2025-01-01 19:42:06 +0200

    up

---
+++ Entity Framework Core Essentials for ASP.NET/ExtensionMethodsEX/ExtensionMethodsEX/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExtensionMethodsEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "this is a test";
+
+            string result = StringExtensions.ToWeirdCase(test);
+
+            Console.WriteLine(result);
+
+
+            Console.WriteLine(test.ToWeirdCase2());
+        }
+    }
+}
---
+++ Entity Framework Core Essentials for ASP.NET/ExtensionMethodsEX/ExtensionMethodsEX/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿namespace ExtensionMethodsEX
+{
+    public static class StringExtensions
+    {
+        public static string ToWeirdCase(string str) // without the key word => this 
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2==0)
+                {
+                    result += str[i].ToString().ToUpper();
+                }
+                else
+                {
+                    result += str[i].ToString().ToLower();
+                } 
+            }
+
+            return result;
+        }
+
+        public static string ToWeirdCase2(this string str) // with the key word => this the method turns in to extension method
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result += str[i].ToString().ToUpper();
+                }
+                else
+                {
+                    result += str[i].ToString().ToLower();
+                }
+            }
+
+            return str;
+        }
+    }
+}
