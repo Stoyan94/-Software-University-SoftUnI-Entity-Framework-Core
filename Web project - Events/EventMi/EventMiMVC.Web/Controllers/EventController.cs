fbb3cf090e1ed82fcd9f23d82dd32a3d48f41aa3
Stoyan <podlobg@gmail.com>
2025-01-15 22:56:54 +0200

    up

--- Web project - Events/EventMi/EventMiMVC.Web/Controllers/EventController.cs
+++ Web project - Events/EventMi/EventMiMVC.Web/Controllers/EventController.cs
@@ -1,7 +1,11 @@
-﻿using EventMi.Web.Services.Data.Contracts;
+﻿using System.Globalization;
+using EventMi.Web.Services.Data.Contracts;
 using EventMi.Web.ViewModels.Event;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Runtime.Intrinsics.X86;
+using System.Runtime.Serialization;
 
 namespace EventMiMVC.Web.Controllers
 {
@@ -32,46 +36,96 @@ namespace EventMiMVC.Web.Controllers
             if (!ModelState.IsValid)
             {
                 return View(model); // Return the view with the model if the model state is not valid
-                                    // This will display the validation errors to the user
-            }
-
-            if (!ModelState.IsValid)
-            {
-                return View(model); // Return the view with the model if the model state is not valid
                 // This will display the validation errors to the user
             }
 
-            
-            // This will display the data that the user entered in the form
-            bool addSuccess = await eventService.AddEvenet(model); // Call the AddEvenet method of the eventService
+            bool isStartDateValid = DateTime.TryParse(model.StartDate, CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out DateTime startDate);
 
-            if (!addSuccess)
+
+            if (!isStartDateValid)
             {
-                ModelState.AddModelError(nameof(model.StartDate), "Start or End date is not in correct format!");
-                return View(model); 
-                
-                // Return the view with the model if the event could not be added
-                //DateTime startDate = DateTime.Parse(model.StartDate);
-                //DateTime endDate = DateTime.Parse(model.EndDate);
-                //if (startDate < endDate)
-                //{
-                //    ModelState.AddModelError(nameof(model.StartDate), "The start date must be earlier than the end date!");
-                //    return View(model);
-                //}
-                //else
-                //{
-                //    ModelState.AddModelError(nameof(model.EndDate), "The end date must be later than the start date!");
-                //    return View(model);
-                //}
-
-                // This will display an error message to the user
+                ModelState.AddModelError(nameof(model.StartDate), "Invalid Start Date Format");
+                return View(model);
             }
 
-          
+            bool isEndDateValid = DateTime.TryParse(model.EndDate, CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out DateTime endDate);
+            if (!isEndDateValid)
+            {
+                ModelState.AddModelError(nameof(model.EndDate), "Invalid End Date Format");
+                return View(model);
+            }
+
+
+            // This will display the data that the user entered in the form
+            await eventService.AddEvenet(model, startDate, endDate ); // Call the AddEvenet method of the eventService
+            
 
             return RedirectToAction("Index", "Home");
             // Redirect the user to the home page if the event was added successfully
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            try
+            {
+                //we check for error because the user may pass a non - existent id and we in service use First() and make sure it won't return null
+                EditEventFormModel eventModel = await eventService.GetEventById(id.Value);
+
+                return View(eventModel); 
+
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Edit(int id, EditEventFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            bool isStartDateValid = DateTime.TryParse(model.StartDate, CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out DateTime startDate);
+
+
+            if (!isStartDateValid)
+            {
+                ModelState.AddModelError(nameof(model.StartDate), "Invalid Start Date Format");
+                return View(model);
+            }
+
+            bool isEndDateValid = DateTime.TryParse(model.EndDate, CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out DateTime endDate);
+            if (!isEndDateValid)
+            {
+                ModelState.AddModelError(nameof(model.EndDate), "Invalid End Date Format");
+                return View(model);
+            }
+
+            try
+            {
+                await eventService.EditEventById(id, model, startDate, endDate);
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+        
     }
 }
