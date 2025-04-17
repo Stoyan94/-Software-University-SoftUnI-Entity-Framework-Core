52f2091d9411ed80b7dbbc2c580b843de349140d
Stoyan <podlobg@gmail.com>
2025-04-17 13:52:01 +0300

    initialize method Save Changes in DbContext

--- OrmMnini 2.0/MiniORM/DbContext.cs
+++ OrmMnini 2.0/MiniORM/DbContext.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using Microsoft.Data.SqlClient;
+using System.Collections;
+using System.Reflection;
+using static MiniORM.ErrorMessages;
 
 namespace MiniORM
 {
@@ -36,6 +39,61 @@ namespace MiniORM
             typeof(DateTime),
             typeof(Guid)
         };
+
+        public void SaveChanges()
+        {
+            IEnumerable<object> dbSetsObjects = this.dbSetProperties
+                .Select(edb => edb.Value.GetValue(this)!)
+                .ToArray();
+
+            foreach (IEnumerable<object> dbSet in dbSetsObjects)
+            {
+                IEnumerable<object> invalidEntities = dbSet
+                    .Where(e => !this.IsObjectValid(e))
+                    .ToArray();
+
+                if (invalidEntities.Any())
+                {
+                    throw new InvalidOperationException(string.Format(InvalidEntitiesInDbSetMessage,
+                        invalidEntities.Count(),dbSet.GetType().Name));
+                }
+            }
+
+            using (new ConnectionManager(this.dbConnection))
+            {
+                using SqlTransaction transaction = this.dbConnection
+                    .StartTransaction();
+
+                foreach (IEnumerable dbSet in dbSetsObjects)
+                {
+                    MethodInfo persistMethod = typeof(DbContext)
+                        .GetMethod("Persist", BindingFlags.NonPublic | BindingFlags.Instance)!
+                        .MakeGenericMethod(dbSet.GetType());
+
+                    try
+                    {
+                        try
+                        {
+                            persistMethod.Invoke(this, new object[] { dbSet });
+                        }
+                        catch (TargetInvocationException tie)
+                            when (tie.InnerException != null)
+                        {
+
+                            throw tie.InnerException;
+                        }
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("Performing Rollback due to Exception!!!");
+                        transaction.Rollback();
+                        throw;
+                    }
+                    
+                    transaction.Commit();
+                }
+            }
+        }
 
         private IDictionary<Type, PropertyInfo> DiscoverDbSet()
         {
--- OrmMnini 2.0/MiniORM/ErrorMessages.cs
+++ OrmMnini 2.0/MiniORM/ErrorMessages.cs
@@ -4,5 +4,8 @@
     {
         public static string PrimaryKeyNullErrorMessage =
             "The primary keys cannot have null values.";
+
+        public static string InvalidEntitiesInDbSetMessage =
+            @"{0} Invalid Entities Found in {1}!";
     }
 }
